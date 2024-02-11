dd05ca8d47d5e680868d8cbc71331156af8bbd50
okrykey <o9mura2000z@gmail.com>
2024-02-11 23:16:33 +0900

    feat: Add shadcn-ui button to test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -39,6 +40,7 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <Button>test</Button>
     </div>
   );
 }
