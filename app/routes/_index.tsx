a3d0d5232208efebb7358d6ff963c78e9a1228e4
okrykey <o9mura2000z@gmail.com>
2024-02-16 23:27:30 +0900

    feat: Build sign-in and sign-up pages and Protect pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import { UserButton } from "@clerk/remix";
+import { getAuth } from "@clerk/remix/ssr.server";
+import {
+  redirect,
+  type LoaderFunction,
+  type MetaFunction,
+} from "@remix-run/node";
 import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
@@ -11,10 +17,19 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader: LoaderFunction = async (args) => {
+  const { userId } = await getAuth(args);
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  return {};
+};
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <UserButton afterSignOutUrl="/" />
       <ul>
         <li>
           <a
